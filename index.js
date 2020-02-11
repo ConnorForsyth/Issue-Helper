4f4ca8ec4a750ed912164b172a87c6013201e764
Glitch (connorforsyth-issue-helper) <none>
2020-02-11 15:44:06 +0000

    🏮🚔 Updated with Glitch

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ module.exports = app => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-    const params = context.issue({ body: 'Hello World!', title })
+    const params = context.issue({ body: 'Hello World!' })
     // Post a comment on the issue
     return context.github.issues.createComment(params)
   })
 }
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
