d746dc20fac053bb43c1902ed4764230addfa53d
Glitch (scintillating-geometry-oy61ygktpm) <none>
2020-02-20 16:01:43 +0000

    Overhauled the app into a stable condition

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ module.exports = (app) => {
   app.log('Yay! The app was loaded!')
 
   // example of probot responding 'Hello World' to a new issue being opened
-  app.on("issues.opened", async context => {
+  app.on('issues.opened', async context => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    //if ("issues.labeled" == "stuck") {
-      const params = context.issue({ body: "Hello World!" });
-
-      // Post a comment on the issue
-      return context.github.issues.createComment(params);
-    /*} else {
-      const params = context.issue({ body: "The label has not worked" });
-
-      // Post a comment on the issue
-      return context.github.issues.createComment(params);
-    }*/
-  });
-};
+    //When referring to the event used context.payload
+    app.log(context.payload.issue.title)
+    
+    const params = context.issue({body: context.payload.issue.title})
+    
+    //First we need to get the payload, then take the title, compare it with generic errors
+    //if no match, use stack exchange
+    
+    
+    
+    
+    
+    
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
+}
